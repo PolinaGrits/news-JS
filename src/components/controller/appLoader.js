51141cf26141b1c1d604e4fbca601727aaed8d05
Polina <polinka256@gmail.com>
2022-07-03 12:17:02 +0300

    feat: add API

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '709eb7aec53b4bce98866de8523eee4c', // получите свой ключ https://newsapi.org/
         });
     }
 }
