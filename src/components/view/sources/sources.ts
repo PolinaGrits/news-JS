151579fa5822c5a419552323151f66075e734d9c
Polina <polinka256@gmail.com>
2022-07-04 15:57:54 +0300

    feat: file soarces  changed

---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,31 @@
+import './sources.css';
+
+interface ISource{
+  category: string,
+  country: string,
+  description: string,
+  id: string,
+  language: string
+  name: string,
+  url: string
+}
+
+class Sources {
+  draw(data: Array<ISource>) {
+    const fragment = document.createDocumentFragment() as DocumentFragment;
+    const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+    data.forEach((item: ISource) => {
+      const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+
+      (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+      (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
+
+      fragment.append(sourceClone);
+    });
+
+    (<HTMLDivElement>document.querySelector('.sources')).append(fragment);
+  }
+}
+
+export default Sources;
