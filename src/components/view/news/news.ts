08262ee3338dc8d516212ebee2073ca98390b4b1
Polina <polinka256@gmail.com>
2022-07-04 14:39:26 +0300

    feat: file news changed

---
+++ src/components/view/news/news.ts
@@ -0,0 +1,70 @@
+import './news.css';
+
+interface INews {
+  author: string,
+  content: string,
+  description: string,
+  publishedAt: string,
+  title: string,
+  url: string,
+  urlToImage: string,
+  source:{
+    id: string,
+    name: string
+  }
+}
+
+interface IArticles{
+  source: 
+  {
+    id: string,
+    name: string
+  },
+  author: string,
+  title: string,
+  description: string,
+  url: string,
+  urlToImage: string,
+  publishedAt: string,
+  content: string
+}
+
+
+
+class News {
+  draw(data: Array<IArticles>) {
+    const news = data.length >= 10 ? data.filter((_item: INews, idx: number) => idx < 10) : data;
+    
+    const fragment = document.createDocumentFragment() as DocumentFragment;
+    const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement; //
+
+    news.forEach((item: INews, idx: number) => {
+      const newsClone = newsItemTemp.content.cloneNode(true) as HTMLElement; 
+   
+      if (idx % 2) (<HTMLDivElement>newsClone.querySelector('.news__item')).classList.add('alt');
+
+      (<HTMLDivElement>newsClone.querySelector('.news__meta-photo')).style.backgroundImage = `url(${
+        item.urlToImage || 'img/news_placeholder.jpg'
+      })`;
+      (<HTMLDivElement>newsClone.querySelector('.news__meta-author')).textContent = item.author || item.source.name;
+      (<HTMLDivElement>newsClone.querySelector('.news__meta-date')).textContent = item.publishedAt
+        .slice(0, 10)
+        .split('-')
+        .reverse()
+        .join('-');
+
+      (<HTMLDivElement>newsClone.querySelector('.news__description-title')).textContent = item.title;
+      (<HTMLDivElement>newsClone.querySelector('.news__description-source')).textContent = item.source.name;
+      (<HTMLDivElement>newsClone.querySelector('.news__description-content')).textContent = item.description;
+      (<HTMLDivElement>newsClone.querySelector('.news__read-more a')).setAttribute('href', item.url);
+
+      fragment.append(newsClone);
+    });
+    
+
+    (<HTMLDivElement>document.querySelector('.news')).innerHTML = '';
+    (<HTMLDivElement>document.querySelector('.news')).appendChild(fragment);
+  }
+}
+
+export default News;
